6b9ea028b6d72c320e00a74c8fe15ba851765c26
yangshijun <yangshijunxxx@gmail.com>
2023-11-02 23:16:30 +0800

    feat: 骨架屏

--- pages/chartRoom/index.vue
+++ pages/chartRoom/index.vue
@@ -51,7 +51,6 @@ export default {
     goEarth() {
       uni.navigateTo({
         url: "/pages/earth/index",
-        // 动画效果
         animationType: "pop-in",
         animationDuration: 200,
         complete: function (res) {
